2a6b157024cb5899ee2ee552a464392d27e1f60c
emamagic <developermagic7@gmail.com>
2021-07-20 19:32:53 +0430

    edit

--- app/src/main/java/com/danovin/app/util/Const.kt
+++ app/src/main/java/com/danovin/app/util/Const.kt
@@ -2,7 +2,7 @@ package com.danovin.app.util
 
 object Const {
 
-    const val URL = "https://app.dnvn.ir"
+    const val URL = ""
     const val PREF_DEVICE_TOKEN_KEY = "device_token"
     const val PREF_NAME = "firebase_pref"
 
